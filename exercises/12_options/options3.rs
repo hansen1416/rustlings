1e917af2146702b92d315d5dfa5be701d143e243
Hanson <105476@grundfos.com>
2024-04-29 10:54:15 +0800

    1

--- exercises/12_options/options3.rs
+++ exercises/12_options/options3.rs
@@ -3,8 +3,6 @@
 // Execute `rustlings hint options3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 struct Point {
     x: i32,
     y: i32,
@@ -14,7 +12,9 @@ fn main() {
     let y: Option<Point> = Some(Point { x: 100, y: 200 });
 
     match y {
-        Some(p) => println!("Co-ordinates are {},{} ", p.x, p.y),
+        // partial move occurs because value has type `Point`, which does not implement the `Copy` trait
+        // borrow this binding in the pattern to avoid moving the value by add `ref`
+        Some(ref p) => println!("Co-ordinates are {},{} ", p.x, p.y),
         _ => panic!("no match!"),
     }
     y; // Fix without deleting this line.
